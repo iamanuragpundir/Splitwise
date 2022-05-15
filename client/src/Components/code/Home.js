c97fa412ef0800f69709e5fa24cd3ee4eb954d58
Ashfiya Hussain <ashfiyahussain04@gmail.com>
2022-05-15 23:50:28 +0530

    Landing

--- client/src/Components/code/Home.js
+++ client/src/Components/code/Home.js
@@ -1,35 +1,81 @@
-import React from 'react'
-import background from '../../assets/images/background.jpg'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import Trip from '../../assets/images/Trip.png'
+import Partner from '../../assets/images/Partner.png'
+import Housemates from '../../assets/images/Housemates.png'
+import Anyone from '../../assets/images/Anyone.png'
 import '../styles/Home.css'
+import Gray from '../../assets/images/Gray.png'
+import Orange from '../../assets/images/Orange.png'
+import Green from '../../assets/images/Green.png'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Home(props) {
 	return (
-		<div className="landing" style={{ backgroundImage: `url(${background})` }}>
-			<div className='content'>
-				<Row>
-					<Col sm={1}>&nbsp;</Col>
-					<Col>
-						<Container>
-							<h1>Splitwise</h1>
-							<h3 className='texts'>Less stress when sharing expenses with Housemates
-							</h3>
-							<p className="text">
-								Keep track of your shared expenses and balances
-								{' '}
-								<br />
-								with housemates, trips, groups, friends, and family.
-							</p>
-						</Container>
-					</Col>
-					<Col>&nbsp;</Col>
-				</Row>
+		<div className='landing'>
+			<Carousel variant="dark">
+				<Carousel.Item interval={2500}>
+					<img
+						className="d-block w-100"
+						src={Trip}
+						alt="First slide"
+					/>
+				</Carousel.Item>
+				<Carousel.Item interval={2500}>
+					<img
+						className="d-block w-100"
+						src={Partner}
+						alt="Second slide"
+					/>
+				</Carousel.Item>
+				<Carousel.Item interval={2500}>
+					<img
+						className="d-block w-100"
+						src={Housemates}
+						alt="Third slide"
+					/>
+				</Carousel.Item>
+				<Carousel.Item interval={2500}>
+					<img
+						className="d-block w-100"
+						src={Anyone}
+						alt="Third slide"
+					/>
+				</Carousel.Item>
+			</Carousel>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			<div>
+				<img src={Gray} className='gray' />
+			</div>
+
+			<div>
+				<img src={Orange} className='orange' />
+			</div>
+			<div>
+				<img src={Gray} className='gray' />
+			</div>
+			<div>
+				<img src={Green} className='green' />
 			</div>
 		</div>
 
 
-
 	)
+
 
 }
 
